b34e30fb228eb7e7c25c44ba5da577e33ee0a06f
JulioV <julio.valenzuela20@inacapmail.cl>
2025-12-07 14:05:17 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
+  base: '/skillswap-front/',
   plugins: [react()],
   server: {
     host: true, // Necesario para Docker
